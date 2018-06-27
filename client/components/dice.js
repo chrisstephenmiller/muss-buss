78decbf40c71ceac11a0bd5cdfca3fd7f6e83e3e
chrisstpehenmiller <csmiller4567@gmail.com>
2018-06-27 00:04:19 -0400

    die vs dice components

--- client/components/dice.js
+++ client/components/dice.js
@@ -1,26 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Die } from '../components'
 
-import d1 from '../../assets/d1.png'
-import d2 from '../../assets/d2.png'
-import d3 from '../../assets/d3.png'
-import d4 from '../../assets/d4.png'
-import d5 from '../../assets/d5.png'
-import d6 from '../../assets/d6.png'
-
-export const Dice = props => {
+const Dice = props => {
 
   const { dice } = props
-  const dieImages = [d1, d2, d3, d4, d5, d6]
   return (
-    <div>
-      {dice.map((die, idx) => {
-        return (
-          <img src={dieImages[die.value - 1]}
-            alt={`die-${die.value}`}
-            key={die.id} />
-        )
-      })}
+    <div style={{ display: `flex` }}>
+      {dice.map((die => {
+        return <Die key={die.id} die={die} />
+      }))}
     </div>
   )
 }
--- server/api/dice.js
+++ server/api/dice.js
@@ -7,7 +7,16 @@ router.get('/', (req, res, next) => {
   try {
     const turn = new Turn
     const dice = turn.dice
-    res.json(dice)
+    res.send(dice)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:die', (req, res, next) => {
+  try {
+    const die = req.params.die
+    res.json(die)
   } catch (err) {
     next(err)
   }
