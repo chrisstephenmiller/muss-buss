c627e1972443ac053d61267a0832a17989ceabc7
chrisstpehenmiller <csmiller4567@gmail.com>
2018-07-05 00:51:15 -0700

    great game setup form

--- client/components/new-game.js
+++ client/components/new-game.js
@@ -8,41 +8,66 @@ class NewGame extends Component {
     super()
 
     this.state = {
-      numPlayers: [1,2]
+      winScore: 10000,
+      numPlayers: [1, 2, 3, 4],
+      name1: `Player 1`,
+      name2: `Player 2`,
+      name3: `Player 3`,
+      name4: `Player 4`,
     }
   }
 
-  numNames = (event) => {
-    const num = event.target.value
-    const numPlayers = []
-    for (let i = 0; i < num; i++) { numPlayers.push(i) }
-    this.setState({ numPlayers })
+  update = event => {
+    const { name, value } = event.target
+    if (name === `numPlayers`) {
+      const numPlayers = []
+      for (let i = 1; i <= value; i++) {
+        numPlayers.push(i)
+        if (!this.state[`name${i}`]) (this.setState({ [`name${i}`]: `Player ${i}` }))
+      }
+      for (const key in this.state) { if (key.slice(4) > value) delete this.state[key] }
+      this.setState({ numPlayers })
+    } else {
+      this.setState({ [name]: value })
+    }
+  }
+
+  submit = event => {
+    event.preventDefault()
+
+    console.log(this.state)
   }
 
   render() {
     const { startGame } = this.props
     return (
       <div style={{ display: `flex` }}>
-        <form name="numPlayers">
+        <form name="players" onChange={this.update} onSubmit={this.submit}>
           <label>
-            How many players? <select onChange={this.numNames}>
-              {[2,3,4,5,6,7,8].map(num => <option key={num} value={num}>{num}</option>)}
+            Points to win: <select name="winScore" defaultValue="10000">
+              {[`5000`, `7500`, `10000`, `20000`].map(score => <option key={score} value={score}>{score}</option>)}
+            </select>
+          </label>
+          <label>
+            Number of players: <select name="numPlayers" defaultValue="4">
+              {[2, 3, 4, 5, 6, 7, 8].map(num => <option key={num} value={num}>{num}</option>)}
             </select>
           </label>
           {this.state.numPlayers.map(num => {
             return (
-              <label key={num} style={{ whiteSpace: `pre` }}>
-                Name: <input type="text" name="name" />
+              <label key={`name${num}`}>
+                Name: <input type="text" name={`name${num}`} placeholder={`Player ${num}`} />
               </label>
             )
           })}
-          <input type="submit" value="Submit" />
+          <label>
+            <input type="submit" value="Start game!" />
+          </label>
         </form>
       </div>
     )
   }
 }
-
 
 const mapState = state => {
   const { dice, game } = state
