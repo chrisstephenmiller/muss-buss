0e77206712a7fccd530b9d6f712712e45a7f9ae8
cmiller <christopher.miller@here.com>
2019-08-14 20:41:20 -0500

    websockets fix

--- client/socket.js
+++ client/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 
-const socket = io(window.location.origin)
+const socket = io(window.location.origin, { transports: ['websocket'] })
 
 socket.on('connect', () => {
   console.log('Connected!')
